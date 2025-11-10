f3241a58ed7c9c07bd9661749e23d80844da1483
Yozora <yozora391@yandex.com>
2025-11-10 16:32:11 +0300

    params

--- pybuild.bat
+++ pybuild.bat
@@ -1,6 +1,6 @@
 @echo off
 :start
 gcc -E -x c -o JPtracks.nml src/index.pnml
-nmlc -c JPtracks.nml -o "D:\DocumentsWin\OpenTTD\newgrf\JPtracks.grf" -o JPtracks.grf -t custom_tags.txt
+nmlc -p LEGACY -c JPtracks.nml -o "D:\DocumentsWin\OpenTTD\newgrf\JPtracks.grf" -o JPtracks.grf -t custom_tags.txt
 pause
 goto start
